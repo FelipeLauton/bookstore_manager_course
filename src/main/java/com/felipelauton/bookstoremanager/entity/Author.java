950d21a540f4ecff7a9762a876c4acea03c43afc
Felipe Lauton <felipelauton021@gmail.com>
2021-10-12 18:18:50 -0300

    commit: entity created

---
+++ src/main/java/com/felipelauton/bookstoremanager/entity/Author.java
@@ -0,0 +1,18 @@
+package com.felipelauton.bookstoremanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
