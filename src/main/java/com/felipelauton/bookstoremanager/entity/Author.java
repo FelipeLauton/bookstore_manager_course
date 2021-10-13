f68358b79778c92b770096e6736056af93fe2536
Felipe Lauton <felipelauton021@gmail.com>
2021-10-13 11:40:16 -0300

    Developed project entities

--- src/main/java/com/felipelauton/bookstoremanager/entity/Author.java
+++ src/main/java/com/felipelauton/bookstoremanager/entity/Author.java
@@ -1,9 +1,10 @@
 package com.felipelauton.bookstoremanager.entity;
 
+
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
-
 public class Author {
 
     @Id
@@ -15,4 +16,47 @@ public class Author {
 
     @Column(nullable = false)
     private Integer age;
+
+    public Author(){
+
+    }
+
+    public Author(Long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
--- src/main/java/com/felipelauton/bookstoremanager/entity/Book.java
+++ src/main/java/com/felipelauton/bookstoremanager/entity/Book.java
@@ -27,4 +27,64 @@ public class Book {
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     @JoinColumn(name ="author_id")
     private Author author;
+
+    public Book(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public Integer getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(Integer chapter) {
+        this.chapter = chapter;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
